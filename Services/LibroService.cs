712155244d45ef636e068654a61125d643038363
cisco <cisco@UPEAULA-31720>
2024-06-17 11:16:13 -0500

    Agregar archivos de proyecto.

---
+++ Services/LibroService.cs
@@ -0,0 +1,26 @@
+﻿using AnahiQuezadaEjemploMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnahiQuezadaEjemploMVVM.Services
+{
+    public class LibroService : ILibroService
+    {
+        public List<Libro> ObtenerLibro()
+        {
+            return new List<Libro>
+            {
+            new Libro { Titulo = "Libro1", Autor = "autor1" },
+            new Libro { Titulo = "Libro2", Autor = "autor2" },
+            new Libro { Titulo = "Libro3", Autor = "autor3" },
+            new Libro { Titulo = "Libro4", Autor = "autor4" },
+            new Libro { Titulo = "Libro5", Autor = "autor5" }
+            };
+
+        }
+
+    }
+}
---
+++ ViewModels/LibroViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AnahiQuezadaEjemploMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnahiQuezadaEjemploMVVM.ViewModels
+{
+    public class LibroViewModel : INotifyPropertyChanged
+    {
+        private Libro _libro;
+        public Libro Libro
+        {  
+            get { return _libro; }
+            set { _libro = value;
+                onPropertyChanged(nameof(Libro));}
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void onPropertyChanged(string propertyName)
+        {
+            PropertyChanged? .Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
